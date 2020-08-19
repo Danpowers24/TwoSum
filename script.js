8854a7a00b7c04e50ab5f1524e51d1918725f13a
Danpowers24 <danpowers610@gmail.com>
2020-08-19 15:43:14 -0700

    work on solution, add comment at the end to remind myself of where I left off

--- script.js
+++ script.js
@@ -11,10 +11,11 @@
 
 
 // define nums array
-const nums = [2, 7, 11, 15]
+// const nums = [2, 7, 11, 15]
+const nums = [3, 7, 8, 3, 9]
 
 // define target
-const target = 17
+const target = 6
 
 const twoSum = function(nums, target) { 
     // define result array
@@ -23,23 +24,29 @@ const twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) { 
         // define currentNum as i 
         let currentNum = nums[i];
-        console.log("in first loop, currentNum is ", currentNum)
-        for (let j = i; j < nums.length; j++) {
+        console.log("in first loop, currentNum (index) is ", currentNum, nums.indexOf(currentNum))
+        for (let j = 0; j < nums.length; j++) {
             let secondNum = nums[j]
-            console.log("in second loop, currentNum and secondNum are ", currentNum, secondNum)
-            // if i === currentNum, then move on to the next number
-            if (currentNum + secondNum == target) {
+            console.log("in second loop, currentNum (index) secondNum (index) are ", currentNum, nums.indexOf(currentNum), secondNum, nums.indexOf(secondNum))
+            // if currentNum + secondNum == target and they are not the same number
+            if (currentNum + secondNum == target && nums[i] != nums[j]) {
                 console.log("in conditional, currentNum ", currentNum, " + secondNum ", secondNum, " = target ", target)
                 // return an array containing the index of currentNum and j
                 result.push(nums.indexOf(currentNum))
                 result.push(nums.indexOf(secondNum))
                 console.log("result is ", result)
                 return result;
-            } 
+            // else if currentNum + secondNum == target and there are 1 or 2 nums in the nums array
+            } else if (currentNum + secondNum == target && nums.length < 3) {
+                result.push(nums.indexOf(currentNum))
+                result.push(1)
+                console.log("result is ", result)
+                return result;
+            }
         }  
     }
 };
 
 twoSum(nums, target)
 
-// This passed LeetCode but only because it solves for the first possible combo, any other combo breaks it
+// This still fails LeetCode tests. for some reason, when the loop gets to the second 3, it checks the first one against the first one. I want it to check the first 3 against the second 3 in the array
